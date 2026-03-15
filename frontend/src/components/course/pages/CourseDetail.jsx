d4a9a348fad02e1892b5fc41bdb3754f12983ca7
Your Name <uttamkumar9234567@gmail.com>
2026-03-15 16:25:27 +0530

    i remove dummy mail

--- frontend/src/components/course/pages/CourseDetail.jsx
+++ frontend/src/components/course/pages/CourseDetail.jsx
@@ -222,12 +222,6 @@ const CourseDetail = () => {
                                     </svg>
                                     06452-220393
                                 </a>
-                                <a href="mailto:admissions@shishuvidyaniketan.edu" className="contact-link">
-                                    <svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                                    </svg>
-                                    admissions@svn.edu
-                                </a>
                             </div>
 
                             <div className="sidebar-divider"></div>
